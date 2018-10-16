f57a719aea3806a1d2b3f6889af598be7997d69e
NOTtrngoldie <mehafbgla000>
2018-10-15 18:23:29 -0700

    AHHHHHHHH

--- NeoBot/src/main/java/com/Neobots2903/Discord/NeoBot/objects/DiscordUser.java
+++ NeoBot/src/main/java/com/Neobots2903/Discord/NeoBot/objects/DiscordUser.java
@@ -1,5 +1,41 @@
 package com.Neobots2903.Discord.NeoBot.objects;
 
-public class DiscordUser {
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement
+public class DiscordUser {
+    private int id;  
+    private String name;  
+    private float salary;  
+  
+public DiscordUser() {}  
+public DiscordUser(int id, String name, float salary) {  
+    super();  
+    this.id = id;  
+    this.name = name;  
+    this.salary = salary;  
+}  
+@XmlAttribute  
+public int getId() {  
+    return id;  
+}  
+public void setId(int id) {  
+    this.id = id;  
+}  
+@XmlElement  
+public String getName() {  
+    return name;  
+}  
+public void setName(String name) {  
+    this.name = name;  
+}  
+@XmlElement  
+public float getSalary() {  
+    return salary;  
+}  
+public void setSalary(float salary) {  
+    this.salary = salary;  
+}  
 }
